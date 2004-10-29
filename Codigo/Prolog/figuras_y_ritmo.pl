0c8decb4b27a129192d9632ebfd34d9e172d89d1
bc24us <bc24us>
2004-10-29 11:54:27 +0000

    pues eso

---
+++ Codigo/Prolog/figuras_y_ritmo.pl
@@ -0,0 +1,12 @@
+%ARCHIVOS PROPIOS CONSULTADOS
+:- consult(['biblio_genaro_fracciones.pl']).
+
+/*divideFigura(Fe, N, Fs)
+divide la figura Fi por el natural N
+in: Fe figura a dividir, cumple es_figura(Fe)
+    N natural que dividirá a la figura, cumple natural(N)
+out: Fs resultado de la division, cumple es_figura(Fs)
+*/
+/* // es la division entera*/
+divideFigura(figura(Ne, De), N, figura(Ns, Ds)) :- natural(N), DAux is De*N
+                       ,simpFraccion(fraccion_nat(Ne, DAux), fraccion_nat(Ns, Ds)).

2daa583af46f3bb7b770cee1d41bc8837ba2fdc5
bc24us <bc24us>
2004-11-03 11:56:34 +0000

    *** empty log message ***

--- Codigo/Haskell/pruebas.hs
+++ Codigo/Haskell/pruebas.hs
@@ -1,1 +1,17 @@
- :load Z:/Haskore/Src/Haskore.hs
+-- :load Z:/Haskore/Src/Haskore.hs
+-- cambiar fromInt por fromIntegral en Src/Bitops.lhs
+module Prueba where
+import ChildSong6
+import TestHaskore
+
+-- cancioncilla :: Music
+cancioncilla = Instr "piano" (Tempo 3 1 (
+	(c 4 wn)
+	:=: ((qn r) :+: (e a wn))	
+	:=: ((qn r) :+: (qn r) :+: (g a wn))
+	:=: ((qn r) :+: (qn r) :+: (qn r) :+: (b a wn))
+	))
+
+prueba :: IO ()
+--prueba = test childSong6
+prueba = test cancioncilla

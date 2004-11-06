214a430f729bf72ed894b9975dc4f7014d29524a
bc24us <bc24us>
2004-11-06 20:14:34 +0000

    pocos avances

--- Codigo/Haskell/pruebas.hs
+++ Codigo/Haskell/pruebas.hs
@@ -3,15 +3,17 @@
 module Prueba where
 import ChildSong6
 import TestHaskore
+import Haskore
 
--- cancioncilla :: Music
-cancioncilla = Instr "piano" (Tempo 3 1 (
+cancioncilla :: Music
+cancioncilla = Instr "piano" (Tempo (3%1) (
 	(c 4 wn)
-	:=: ((qn r) :+: (e a wn))	
-	:=: ((qn r) :+: (qn r) :+: (g a wn))
-	:=: ((qn r) :+: (qn r) :+: (qn r) :+: (b a wn))
+	:=: ((qnr) :+: (e a wn))	
+	:=: ((qnr) :+: (qnr) :+: (g a wn))
+	:=: ((qnr) :+: (qnr) :+: (qnr) :+: (b a wn))
 	))
 
 prueba :: IO ()
 --prueba = test childSong6
 prueba = test cancioncilla
+

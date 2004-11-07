4efaec5b0c498b40f7464a3bf597ced7d34a4d54
bc24us <bc24us>
2004-11-07 21:43:35 +0000

    primera aproximación, versión definitiva

--- Codigo/Haskell/pruebas.hs
+++ Codigo/Haskell/pruebas.hs
@@ -1,19 +1,21 @@
 -- :load Z:/Haskore/Src/Haskore.hs
 -- cambiar fromInt por fromIntegral en Src/Bitops.lhs
 module Prueba where
-import ChildSong6
 import TestHaskore
 import Haskore
+import Basics
+import Ratio
 
 cancioncilla :: Music
 cancioncilla = Instr "piano" (Tempo (3%1) (
-	(c 4 wn)
-	:=: ((qnr) :+: (e a wn))	
-	:=: ((qnr) :+: (qnr) :+: (g a wn))
-	:=: ((qnr) :+: (qnr) :+: (qnr) :+: (b a wn))
+	(c 4 wn [])
+	:=: (qnr :+: (e 4 wn []))	
+	:=: (qnr :+: qnr :+: (g 4 wn []))
+	:=: (qnr :+: qnr :+: qnr :+: (b 4 wn []))
 	))
 
+
+
 prueba :: IO ()
---prueba = test childSong6
 prueba = test cancioncilla
 

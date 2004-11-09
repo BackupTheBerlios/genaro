c6076c982bf2024e123c6638f3b8df8a07b17163
bc24us <bc24us>
2004-11-09 21:39:44 +0000

    estamos en ello

--- Codigo/Haskell/PrologAMidi.hs
+++ Codigo/Haskell/PrologAMidi.hs
@@ -1,9 +1,3 @@
-module PrologAMidi where
-import HaskoreAMidi
-import Haskore
-import Ratio
-
-
 -- PARSER BASADO EN EL ARTICULO: Jeroen Fokker
 -- 				 Functional Parsers
 --				 in: Johan Jeuring and Erik Meijer (eds.)
@@ -11,6 +5,13 @@ import Ratio
 --				     Fst. Int. School on Advanced Functional Programming Techniques
 --				     Tutorial Text
 --				     Springer LNCS 925, pp. 1-23, 1995 
+
+
+module PrologAMidi where
+import HaskoreAMidi
+import Haskore
+import Ratio
+import Parsers
 
 
 -- Lee un archivo de texto con una musica en nuestro formato de prolog y lo transforma en un midi (valores por
@@ -28,11 +29,7 @@ hazMusica cadena = if null resultados
 		   where resultados = parserMusica cadena
 			 resul1     = head resultados
 
-
--- analiza una lista de elementos de tipo simbolo y devuelve una lista de parejas de la forma 
--- (resto, resultado), donde resto es lo que queda por analizar de la entrada y resultado es el resultado del
--- analisis. Es una lista porque es posible que el analisis no sea univoco
-type Parser simbolo resul  = [simbolo] -> [([simbolo], resul)]
+-- parserMusica :: String -> [(String, Music)]
 parserMusica :: Parser Char Music
 -- [(resto, musica)] = parserMusica cadena :
 -- cadena es la entrada del analizador
@@ -40,31 +37,14 @@ parserMusica :: Parser Char Music
 -- musica es el resultado del analisis
 parserMusica _ = [([], cancioncilla)]
 
--- token :: String -> Parser Char String
-token :: Eq [s] => [s] -> Parser s [s]
-token k xs
- | k==take n xs = [(drop n xs, k)]
- | otherwise = []
-	where n = length k
-
--- (resto, token) = dameSigToken Cadena : 
--- cadena es la entrada del analizador
--- resto es lo que queda de cadena por analizar tras emparejar un token de la entrada
--- token es el token que se queria emparejar
-dameSigToken :: String -> [(String, String)]
-dameSigToken cadena = dameSigTokenAcu cadena listaTokens
---dameSigToken = concat [token t cadena | t <- listaTokens]
-
-dameSigTokenAcu :: String -> [String] ->[(String, String)]
-dameSigTokenAcu _ [] = []
-dameSigTokenAcu cadena (t:ts) = if null resul 
-                                then dameSigTokenAcu cadena ts
-				else resul
-			where resul = token t cadena
 
 -- lista de todos de los tokens de la entrada
 listaTokens :: [String]
 listaTokens = ["nota", "altura"]
+
+listaBlancos :: [String]
+listaBlancos =["\n", "\t", " "]
+
 
 -- hazMusica _ = cancioncilla
 -- auxiliar para pruebas

cd67445552558a13c758d6e19f7fc017a3c8b4ec
bc24us <bc24us>
2004-11-29 15:54:40 +0000

    no compila

--- Codigo/Prolog/figuras_y_ritmo.pl
+++ Codigo/Prolog/figuras_y_ritmo.pl
@@ -17,3 +17,19 @@ divideFigura(figura(Ne, De), N, figura(Ns, Ds)) :- natural(N), DAux is De*N
 /*sumaFiguras(Fs1, Fs2, Fresul)*/
 sumaFiguras(figura(N1,D1), figura(N2,D2), figura(Nr, Dr)):- 
 	sumarFracciones(fraccion_nat(N1,D1), fraccion_nat(N2,D2), fraccion_nat(Nr, Dr)).
+
+/**
+* fuerzaEnElCompas(Num, Tipo, Fuerza) devuelve en Fuerza, para el tipo de compás especificado 
+* en Tipo, si la posición Num que indica un numero de redondas, si esta posición es débil
+* ,fuerte o semifuerte dentro de las divisiones del compás
+* @param +Num de tipo float
+* @param +Tipo del compás, pertenece a {binario, ternario, cuaternario}. Por ahora sólo
+* implementado para binario
+* @param -Fuerza pertenece a {fuerte, debil, semifuerte}
+*/
+fuerzaEnElCompas(Num, Tipo, Fuerza)
+/**
+* floatSubeAInt(F, I): auxiliar, devuelve en I un entero generado a partir de F 
+* quitándole los decimales a base de multiplicarlo por 10
+*
+*/

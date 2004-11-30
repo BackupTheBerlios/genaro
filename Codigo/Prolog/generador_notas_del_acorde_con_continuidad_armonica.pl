277c21e3cb3af1f4f43582a59f153977079b4268
roberto_____666 <roberto_____666>
2004-11-30 11:28:18 +0000

    *** empty log message ***

--- Codigo/Prolog/generador_notas_del_acorde_con_continuidad_armonica.pl
+++ Codigo/Prolog/generador_notas_del_acorde_con_continuidad_armonica.pl
@@ -8,6 +8,12 @@
 :-use_module(biblio_genaro_acordes).
 
 
+/**
+* modulo12( +Nota, -NotaSalida)
+* Calcula el modulo 12 de Nota representandola como un valor de 0 a 11 (0 es el La)
+* @param +Nota nota a calcula el modulo 12
+* @param -NotaSalida nota entre 0 y 11
+*/
 modulo12( numNota(N1), numNota(N1) ) :-
 	-1<N1,
 	12>N1.
@@ -20,7 +26,14 @@ modulo12( numNota(N1), numNota(N2) ) :-
 	Aux is N1 - 12,
 	modulo12( numNota(Aux), numNota(N2) ).
 	
-	
+
+/**
+* distancia_alturas( +Altura1, +Altura2, -Distancia )
+* Calcula la distancia en semitonos de la Altura1 a la Altura2.
+* @param +Altura1 altura1
+* @param +Altura2 altura2
+* @param -Distancia numero entero mayor o igual a cero que representa la distancia entre altura1 y altura2
+*/
 distancia_alturas( altura(numNota(N1),octava(O1)), altura(numNota(N2),octava(O2)), Dist ) :-
 	N_aux1 is N1-3,
 	N_aux2 is N2-3,
@@ -30,7 +43,15 @@ distancia_alturas( altura(numNota(N1),octava(O1)), altura(numNota(N2),octava(O2)
 	Aux2 is N_aux4 + (O2 * 12),
 	Dist is abs(Aux1 - Aux2).
 	
-
+/**
+* distancia_acorde( +Acorde1, +Acorde2, -Distancia)
+* Calcula la distancia entre el Acorde1 y el Acorde2. Entendemos por distancia la suma de las distancias en
+* semitonos de cada voz del acorde a la del siguiente acorde. Solo consideramos las cuatro primeras voces
+* para hacerlo compatible con triadas y cuatriadas
+* @param +Acorde1 acorde1
+* @param +Acorde2 acorde2
+* @param -Distancia numero entero mayor o igual a 0 que determina la distancia de Acorde1 con Acorde2
+*/
 distancia_acorde(acorde(Acorde1), acorde(Acorde2), Distancia) :-
 	reverse(Acorde1, Acorde1_aux),
 	reverse(Acorde2, Acorde2_aux),
@@ -44,7 +65,15 @@ distancia_acorde_recursivo( [Altura1 | RestoAlturas1 ] , [Altura2 | RestoAlturas
 	distancia_acorde_recursivo( RestoAlturas1, RestoAlturas2, Dist2, N1 ),
 	Dist is Dist1 + Dist2.
 
-
+/**
+* menor_acorde( +Acorde_referencia, +Acorde1, +Acorde2, -AcordeMenor )
+* Compara la distancia de Acorde1 y Acorde2 con el Acorde_referencia y devuelve en AcordeMenor el que tuviera
+* la menor distancia
+* @param +Acorde_referencia acorde con el que comparamos las distancias
+* @param +Acorde1 acorde1
+* @param +Acorde2 acorde2
+* @param -AcordeMenor acorde1 si tiene menor distancia que acorde2 o viceversa
+*/
 menor_acorde(Acorde_referencia, Acorde1, Acorde2, Acorde2 ) :-
 	distancia_acorde( Acorde_referencia, Acorde1, Dist1 ),
 	distancia_acorde( Acorde_referencia, Acorde2, Dist2 ),
@@ -53,13 +82,31 @@ menor_acorde(Acorde_referencia, Acorde1, Acorde2, Acorde2 ) :-
 menor_acorde( _, Acorde1, _, Acorde1 ).
 
 
+
+/**
+* menor_lista_acordes( +Acorde_referencia, +ListaAcordes, -AcordeMenor )
+* Devuelve el acorde de la ListaAcordes que tiene una distancia menor con el Acorde_referencia
+* @param +Acorde_referencia acorde con el cual comparamos la distancia
+* @param +ListaAcordes lista de acordes en la que buscamos el menor acorde
+* @param -AcordeMenor un acorde que tiene una distancia menor con Acorde_referencia
+*/
 menor_lista_acordes( _, [Acorde], Acorde ) :-
 	!.
 menor_lista_acordes( Acorde_referencia, [Acorde | Resto], Acorde_salida) :-
 	menor_lista_acordes( Acorde_referencia, Resto, Acorde_menor ),
 	menor_acorde(Acorde_referencia, Acorde_menor, Acorde, Acorde_salida ).
-	
 
+
+	
+/**
+* elimina_diferentes( +Acorde_referencia, +Distancia_menor, +ListaEntrada, -ListaAcordes )
+* elimina de la ListaEntrada aquellos acordes cuya distancia con Acorde_referencia sea mayor que
+* Distancia_menor, dejando solamente los que tienen una distancia igual.
+* @param +Acorde_referencia acorde con el cual se compara la distancia
+* @param +Distancia_menor distancia menor de los acordes de ListaEntrada
+* @param +ListaEntrada lista de acordes de entrada
+* @param -ListaAcordes lista con los acordes que tienen la distancia con Acorde_referencia igual a Distancia_menor
+*/
 elimina_diferentes( _, _, [], [] ) :-
 	!.
 elimina_diferentes( Acorde_referencia, Distancia_menor, [Acorde | Resto], [Acorde | Salida] ) :-
@@ -68,26 +115,28 @@ elimina_diferentes( Acorde_referencia, Distancia_menor, [Acorde | Resto], [Acord
 elimina_diferentes( Acorde_referencia, Distancia_menor, [_ | Resto], Salida ) :-
 	elimina_diferentes( Acorde_referencia, Distancia_menor, Resto, Salida  ).
 
-
+/**
+* elegir_aleatoriamente( +Lista, -Elemento )
+* Elige un elemento de la lista Lista aleatoriamente
+* @param Lista lista de la cual se elige un elemento aleatoriamente
+* @param Elemento elemento de la lista elegido aleatoriamente
+*/
 elegir_aleatoriamente( L, A ) :-
 	length( L, N1 ),
 	random(0, N1, N2),
 	nth0(N2, L, A).
 
-/*
-USO:
-	traduce_lista_cifrados(+Progresion,-ListaAcrodes )
-PARAMETROS:
-	Progresion: es una progresion de cifrados
-	ListaAcrodes: lista de acorde resultante de la traduccion a notas de la lista de cifrados Progresion
-DESCRIPCION:
-	Este predicado traduce una lista de cifrados a una lista de acordes, en donde ya estan representadas las
-	alturas de las notas, por el metodo de continuidad armonica. Dicho metodo consisten en buscar la inversion y 
-	la disposicion de cada acorde que haga que el movimiento de cada voz entre acordes sea el menor.
-		El algoritmo consiste en lo siguiente. Primero elegimos una inversion y una disposicion aleatoriamente
-	que nos servira de referencia para el siguiente acorde. Para cada cifrado de la progresion hacemos lo siguiente:
-	calculamos todos los acordes con todas las disposiciones y buscamos los acorden que tengan una distancia
-	menor al anterior acorde. De todas ellas elegimos una aleatoriamente.
+/**
+* traduce_lista_cifrados(+Progresion,-ListaAcrodes )
+* Este predicado traduce una lista de cifrados a una lista de acordes, en donde ya estan representadas las
+* alturas de las notas, por el metodo de continuidad armonica. Dicho metodo consisten en buscar la inversion y 
+* la disposicion de cada acorde que haga que el movimiento de cada voz entre acordes sea el menor.
+* 	El algoritmo consiste en lo siguiente. Primero elegimos una inversion y una disposicion aleatoriamente
+* que nos servira de referencia para el siguiente acorde. Para cada cifrado de la progresion hacemos lo siguiente:
+* calculamos todos los acordes con todas las disposiciones y buscamos los acorden que tengan una distancia
+* menor al anterior acorde. De todas ellas elegimos una aleatoriamente.
+* @param	Progresion es una progresion de cifrados
+* @param	ListaAcrodes lista de acorde resultante de la traduccion a notas de la lista de cifrados Progresion
 */
 traduce_lista_cifrados( progresion([(Cifrado, Figura) | Resto]), [ (Acorde_ref,Figura) | Salida] ) :-
 	findall(A , traduce_cifrado(Cifrado, _, _, A), Lista),

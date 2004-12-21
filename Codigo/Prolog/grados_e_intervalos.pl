5f67b28cb424c58ad7448522171e89d197580677
roberto_____666 <roberto_____666>
2004-12-21 09:27:01 +0000

    *** empty log message ***

--- Codigo/Prolog/grados_e_intervalos.pl
+++ Codigo/Prolog/grados_e_intervalos.pl
@@ -1,4 +1,5 @@
-%DECLARACION DEL MODULO
+
+
 :- module(grados_e_intervalos,[alturaAbsoluta/2
                               ,sumaSemitonos/3
                               ,semitonosAIntervalo/2
@@ -11,17 +12,29 @@
                               ,listaGradosNoDiatonicos/2
                               ,intervaloASemitonos/2]).
 
-%BIBLIOTECAS
+/* Modulos de Prolog */
 :- use_module(library(lists)).
+
+/* Modulos propios */
 :- use_module(biblio_genaro_acordes).
 
-%INTERVALOS SIMPLES
-%usuales
-es_interSimple(interSimple(G)) :- member(G, [i, bii, ii, biii, iii, iv, bv, v, auv, vi, bvii, vii]).
-%raros
-es_interSimple(interSimple(G)) :- member(G, [bbii, bbiii, auii, biv, auiii, auiv, bbvi, bvi, auvi, bviii, auviii ]).
+/**
+* es_interSimple(+-Intervalo)
+* Cierto si Intervalo es un intervalo simple, es decir, que es un intervalo menor que el de un intervalo de octava
+* @param +-Intervalo esta formado con la constructora interSimple(G) donde G es la notacion musical de los intervalos
+*/
+/* Mas comunes */
+es_interSimple(interSimple(G)) :- 
+	member(G, [i, bii, ii, biii, iii, iv, bv, v, auv, vi, bvii, vii]).
+/* Mas raros */
+es_interSimple(interSimple(G)) :- 
+	member(G, [bbii, bbiii, auii, biv, auiii, auiv, bbvi, bvi, auvi, bviii, auviii ]).
 
-%GRADOS
+/**
+* es_grado(+-Grado)
+* Definicion de la estructura grado. Cierto si Grado es un grado
+* @param +-Grado comienza con la constructora grado(G)
+*/
 es_grado(grado(G)) :- es_interSimple(interSimple(G)).
 es_grado(grado(v7 / G)) :- es_grado(grado(G)).
 es_grado(grado(iim7 / G)) :- es_grado(grado(G)).
@@ -34,69 +47,114 @@ es_grado(grado(iim7 / G)) :- es_grado(grado(G)).
  */
 listaGradosNoDiatonicos(jonico, [grado(v7 / _), grado(iim7 / _)]).
 
-/*intervalo: como par formado por un intervalo simple y un natural
-que indica cuantas veces se ha salido de la octava: ej: 5º justa = intervalo(interSimple (v),0), 9ºmenor = intervalo(interSimple (bii),1)
-8ª = intervalo(interSimple (i),1)*/
-es_intervalo(intervalo(G,O)) :- es_interSimple(G), natural(O).
 
-/*convierte una altura a una escala absoluta que empieza a contar desde el la de la octava 0, que es el cero.
- Sólo funciona bien si alturaAbsoluta(A,N) tiene
- in=A tipo altura (cumple es_altura(A))
- out=N entero
+/**
+* es_intervalo(+Intervalo)
+* Definicion de la estructura Intervalo
+* @param +Intervalo consiste en una pareja con la constructora intervalo(G,O). G es un intervalo simple y O indica cuantas
+* 			veces se ha salido de la octava. Ej: 
+*			5º justa = intervalo(interSimple (v),0), 
+*			9º menor = intervalo(interSimple (bii),1),
+* 			8ª justa = intervalo(interSimple (i),1)
 */
-alturaAbsoluta(altura(numNota(N),octava(0)),N).
-alturaAbsoluta(altura(numNota(N),octava(O)),A) :- O>0, O1 is O - 1, alturaAbsoluta(altura(numNota(N),octava(O1)),A2)
-						,A is A2 + 12.
-alturaAbsoluta(altura(numNota(N),octava(O)),A) :- O<0, O1 is O + 1, alturaAbsoluta(altura(numNota(N),octava(O1)),A2)
-						,A is A2 - 12.
-
-/*alturaAbsAAltura(N,A)
-  Realiza la conversion opuesta a alturaAbsoluta(A,N)
-  in: N entero
-  out: A de tipo altura (cumple es_altura(A))
-*/
-alturaAbsAAltura(N, altura(numNota(N),octava(0))) :- N>=0, N<12.
-alturaAbsAAltura(N, altura(numNota(M),octava(O1))):- N>=12, N1 is N - 12
-	,alturaAbsAAltura(N1, altura(numNota(M),octava(O))), O1 is O + 1.
-alturaAbsAAltura(N, altura(numNota(M),octava(O1))):- N<0, N1 is N + 12
-	,alturaAbsAAltura(N1, altura(numNota(M),octava(O))), O1 is O - 1.
-
-/*sumaSemitonos(Ae, N, As)
-  Calcula en As la altura resultado de sumar N semitonos a la altura Ae
- in: Ae cumple es_altura(Ae)
-     N es un entero
- out:As cumple es_altura(As)
-*/
-sumaSemitonos(Ae, N, As) :- alturaAbsoluta(Ae, Ne), Ns is N + Ne, alturaAbsAAltura(Ns, As).
+es_intervalo(intervalo(G,O)) :- 
+	es_interSimple(G), 
+	natural(O).
 
 
 /**
-* sumaIntervaloAGrado(Gi, Ii, Gs) calcula en Gs el grado resultado de moverse el intervalos Ii a partir
+* alturaAbsoluta(+Altura,-AlturaAbsoluta)
+* Convierte una altura representada como una pareja de numNota y Octava en una altura absoluta, es decir, el numero
+* de semitonos contando desde el Do de la octava 0
+* @param +Altura altura, cumple es_altura(Altura)
+* @param -AlturaAbsoluta entero que indica el numero de semitonos desde el Do de la octava 0
+*/
+alturaAbsoluta(altura(numNota(N),octava(0)),N).
+alturaAbsoluta(altura(numNota(N),octava(O)),A) :- 
+	O>0, 
+	O1 is O - 1, 
+	alturaAbsoluta(altura(numNota(N),octava(O1)),A2),
+	A is A2 + 12.
+alturaAbsoluta(altura(numNota(N),octava(O)),A) :- 
+	O<0, 
+	O1 is O + 1, 
+	alturaAbsoluta(altura(numNota(N),octava(O1)),A2),
+	A is A2 - 12.
+
+
+/**
+* alturaAbsAAltura(+Semitonoes,-Altura)
+* Realiza la conversion opuesta a alturaAbsoluta(Altura, Semitonos), es decir, pasa de el numero de
+* semitonos o una altura que cumple es_altura/1
+* @param +Semitonos entero que indica los semitonoes desde Do de la octava 0
+* @param -Altura altura (cumple es_altura(Altura))
+*/
+alturaAbsAAltura(N, altura(numNota(N),octava(0))) :- 
+	N>=0, 
+	N<12.
+alturaAbsAAltura(N, altura(numNota(M),octava(O1))):- 
+	N>=12, 
+	N1 is N - 12,
+	alturaAbsAAltura(N1, altura(numNota(M),octava(O))), 
+	O1 is O + 1.
+alturaAbsAAltura(N, altura(numNota(M),octava(O1))):- 
+	N<0, 
+	N1 is N + 12,
+	alturaAbsAAltura(N1, altura(numNota(M),octava(O))), 
+	O1 is O - 1.
+
+
+/**
+* sumaSemitonos(+AlturaE, +N, -AlturaS)
+* Calcula la suma de la altura AlturaE con N que es el numero de semitonos
+* @param +AlturaE cumple es_altura(AlturaE)
+* @param +N		entero. Numero de semitonoes
+* @param -AlturaS cumple es_altura(AlturaS)
+*/
+sumaSemitonos(Ae, N, As) :- 
+	alturaAbsoluta(Ae, Ne), 
+	Ns is N + Ne, 
+	alturaAbsAAltura(Ns, As).
+
+
+/**
+* sumaIntervaloAGrado(+Gi, +Ii, -Gs) calcula en Gs el grado resultado de moverse el intervalos Ii a partir
 * del grado Gi
 * @param +Gi cumple es_grado(Gi)
 * @param +Ii cumple es_intervalo(Ii)
 * @param -Gs cumple es_grado(Gs)
 */
-sumaIntervaloAGrado(Gi, Ii, Gs) :- gradoAAltura(Gi, Ai), intervaloASemitonos(Ii, Si)
-			,trasponer(Ai, Si, As), alturaAGrado(As, Gs).
-/**
-* gradoRelativoAAbsoluto(Gi, Go) Go es un grado equivalente a Gi pero sin la notacion relativa debida
-* a v7/g o iim7/G
-* @param +Gi cumple es_grado(Gi)
-* @param +Go cumple es_grado(Go)
-*/
-gradoRelativoAAbsoluto(grado(v7 / G), Go) :-
-            !,gradoRelativoAAbsoluto(grado(G), Gaux),sumaIntervaloAGrado(Gaux, intervalo(interSimple(v),0), Go).
-gradoRelativoAAbsoluto(grado(iim7 / G), Go) :-
-            !,gradoRelativoAAbsoluto(grado(G), Gaux),sumaIntervaloAGrado(Gaux, intervalo(interSimple(ii),0), Go).
-gradoRelativoAAbsoluto(Gi, Gi).
+sumaIntervaloAGrado(Gi, Ii, Gs) :- 
+	gradoAAltura(Gi, Ai), 
+	intervaloASemitonos(Ii, Si),
+	trasponer(Ai, Si, As), 
+	alturaAGrado(As, Gs).
+
 
 /**
+* gradoRelativoAAbsoluto(+Gi, -Go) 
+* Go es un grado equivalente a Gi pero sin la notacion relativa debida a v7/g o iim7/G
+* @param +Gi cumple es_grado(Gi)
+* @param -Go cumple es_grado(Go)
+*/
+gradoRelativoAAbsoluto(grado(v7 / G), Go) :-
+	!,
+	gradoRelativoAAbsoluto(grado(G), Gaux),
+	sumaIntervaloAGrado(Gaux, intervalo(interSimple(v),0), Go).
+gradoRelativoAAbsoluto(grado(iim7 / G), Go) :-
+	!,
+	gradoRelativoAAbsoluto(grado(G), Gaux),
+	sumaIntervaloAGrado(Gaux, intervalo(interSimple(ii),0), Go).
+gradoRelativoAAbsoluto(Gi, Gi).
+
+
+/**
+* gradoAAltura(+Grado, -Altura)
 * Sólamente para uso interno en este módulo, gradoAAltura(G,A) hace una correspondencia arbitraria entre
 * grados y alturas sólo para cuentas internas
-* @param +G cumple es_grado(G)
-* @param -A cumple es_altura(A)
-* */
+* @param +Grado cumple es_grado(G)
+* @param -Altura cumple es_altura(A)
+*/
 gradoAAltura(grado(i), altura(numNota(3),octava(0))).
 gradoAAltura(grado(bii), altura(numNota(4),octava(0))).
 gradoAAltura(grado(ii), altura(numNota(5),octava(0))).
@@ -113,11 +171,12 @@ gradoAAltura(grado(v7 / G), A) :- gradoAAltura(grado(G), A1), trasponer(A1, 7, A
 gradoAAltura(grado(iim7 / G), A) :- gradoAAltura(grado(G), A1), trasponer(A1, 2, A).
 
 /**
+* alturaAGrado(+Altura, -Grado)
 * Sólamente para uso interno en este módulo, alturaAGrado(A,G) hace una correspondencia arbitraria entre
 * grados y alturas sólo para cuentas internas
-* @param +A cumple es_altura(A)
-* @param -G cumple es_grado(G)
-* */
+* @param +Altura cumple es_altura(A)
+* @param -Grado cumple es_grado(G)
+*/
 alturaAGrado(altura(numNota(3),_), grado(i)).
 alturaAGrado(altura(numNota(4),_), grado(bii)).
 alturaAGrado(altura(numNota(5),_), grado(ii)).
@@ -132,7 +191,8 @@ alturaAGrado(altura(numNota(1),_), grado(bvii)).
 alturaAGrado(altura(numNota(2),_), grado(vii)).
 
 /**
-* intervaloASemitonos(I,S) devuelve en S el numero de semitonos q comprende el intervalo I
+* intervaloASemitonos(+I, -S) 
+* Devuelve en S el numero de semitonos q comprende el intervalo I
 * @param +I cumple es_intervalo(I)
 * @param -S es un natural
 */
@@ -151,11 +211,11 @@ intervaloASemitonos(intervalo(interSimple(vii),0),11).
 intervaloASemitonos(intervalo(Is,N),S) :- N>0, N1 is N - 1
 		, intervaloASemitonos(intervalo(Is,N1),S1), S is S1 + 12.
 
-/*semitonosAIntervalo(S,I) : S es el numero (natural!!!!) de semitonos del intervalo I
--es biyectiva? pq sólo tiene en cuenta los intervalos simples usuales
--solo funiona bien con
-	in=S natural q indica un numero de semitonos
-	out=I	tipo intervalo correpondiente
+/**
+* semitonosAIntervalo(+S,-I)
+* Cumple que es S es el numero natural de semitonos del intervalo I
+* @param +S natural que indica el numero de semitonoes
+* @param -I tipo intervalo que corresponde a S
 */
 semitonosAIntervalo(0, intervalo(interSimple(i),0)).
 semitonosAIntervalo(1, intervalo(interSimple(bii),0)).
@@ -169,14 +229,27 @@ semitonosAIntervalo(8, intervalo(interSimple(auv),0)).
 semitonosAIntervalo(9, intervalo(interSimple(vi),0)).
 semitonosAIntervalo(10, intervalo(interSimple(bvii),0)).
 semitonosAIntervalo(11, intervalo(interSimple(vii),0)).
-semitonosAIntervalo(S, intervalo(G,O)) :- S>11, S1 is S - 12, 	semitonosAIntervalo(S1, intervalo(G,O1)),O is O1 + 1.
+semitonosAIntervalo(S, intervalo(G,O)) :- 
+	S>11, 
+	S1 is S - 12,
+	semitonosAIntervalo(S1, intervalo(G,O1)),
+	O is O1 + 1.
 
-/*el intervalo entre dos alturas A1 y A2 se obtiene con dameIntervalo(A1, A2,I).
-In =  A1 tipo altura (cumple es_altura(A))
-	A2 de tipo altura (cumple es_altura(A))
-Out = I intervalo entre ambos
-Funciona para A1 más bajo q A2 y viceversa
+/**
+* dameIntervalo(+Altura1, +Altura2, -Intervalo)
+* Calcula el intervalo entre las alturas 1 y 2
+* @param +Altura1 cumple es_altura(Altura1)
+* @param +Altura2 cumple es_altura(Altura2)
+* @param -Intervalo intervalor entre ambas alturas
 */
-dameIntervalo(A1, A2, I) :- alturaAbsoluta(A1, AA1),alturaAbsoluta(A2, AA2),
-					 IA is AA2 - AA1,	semitonosAIntervalo(IA,I),!.
-dameIntervalo(A1, A2, I) :- dameIntervalo(A2, A1, I).
+dameIntervalo(A1, A2, I) :- 
+	alturaAbsoluta(A1, AA1),
+	alturaAbsoluta(A2, AA2),
+	IA is AA2 - AA1,
+	semitonosAIntervalo(IA,I),
+	!.
+dameIntervalo(A1, A2, I) :- 
+	dameIntervalo(A2, A1, I).
+
+
+
--- Codigo/Prolog/representacion_prolog_haskore.pl
+++ Codigo/Prolog/representacion_prolog_haskore.pl
@@ -1,4 +1,4 @@
-%DECLARACION DEL MODULO
+
 :- module(representacion_prolog_haskore,[
 	es_musica/1,
 	es_nota/1,
@@ -9,46 +9,104 @@
 	es_figura/1
 ]).
 
-%BIBLIOTECAS
+/* Modulos de Prolog */
 :- use_module(library(lists)).
 
-%ARCHIVOS PROPIOS CONSULTADOS
+/* Modulos propios */
 :- use_module(biblio_genaro_fracciones).
 
-%OPERADORES
-%composicion secuencial (asocia a dcha)
+/* Operadores infijos*/
+/* composicion secuencial (asocia a dcha) */
 :- op(100, xfy,[:+:]).
-%composicion paralela (asocia a dcha)
+/* composicion paralela (asocia a dcha) */
 :- op(101, xfy, [:=:]).
 
-%MUSICA
-es_musica(X) :- es_nota(X).
-es_musica(X) :- es_silencio(X).
+/**
+* es_musica(+M)
+* Definicion de la estructura Musica. Esta estructura es una imitacion del tipo Music de Haskore de Paul Hudak
+* realizada en Haskell. Una musica es:
+* una nota: cumple es_nota(M)
+* un silencio: cumple es_silencio(M)
+* M1 :+: M2 : cumplen es_musica(M1), es_musica(M2)
+* M1 :=: M2 : cumplen es_musica(M1), es_musica(M2)
+* tempo((Num, Den), M) : cumplen es_musica(M), es_fraccionNat(Num,Den)
+* tras(Ent,M) : cumplen es_musica(M), integer(Ent)
+* inst(Ins, M) : cumplen es_musica(M), es_instrumento(Ins)
+* @param +M una musica
+*/
+es_musica(X) :- 
+	es_nota(X).
+es_musica(X) :- 
+	es_silencio(X).
 %composicion secuencial
-es_musica(X :+: Y) :- es_musica(X), es_musica(Y).
+es_musica(X :+: Y) :- 
+	es_musica(X), 
+	es_musica(Y).
 %composicion paralela
-es_musica(X :=: Y) :- es_musica(X), es_musica(Y).
+es_musica(X :=: Y) :- 
+	es_musica(X), 
+	es_musica(Y).
 %musica cuyo tempo se multiplica por F
-es_musica(tempo((N, D),M)) :- es_fraccionNat(N,D), es_musica(M).
+es_musica(tempo((N, D),M)) :- 
+	es_fraccionNat(N,D), 
+	es_musica(M).
 %musica que se subida (traspuesta) el numero de semitonos E
-es_musica(tras(E,M)) :- integer(E), es_musica(M).
+es_musica(tras(E,M)) :- 
+	integer(E), 
+	es_musica(M).
 %musica tocada con un instrumento determinado
-es_musica(inst(I,M)) :- es_instrumento(I), es_musica(M).
-	%CASOS BASE
+es_musica(inst(I,M)) :- 
+	es_instrumento(I), 
+	es_musica(M).
+
+/**
+* es_nota(+Nota)
+* Definicion de la estructura Nota. Basicamente es una pareja formada por el functor nota/2 con una altura
+* y una figura
+* @param +Nota una nota
+*/
 es_nota(nota(A,F)):- es_altura(A), es_figura(F).
+
+/**
+* es_silencio(+Silencio)
+* Definicion de la estructura Silencio. Basicamente es una figura con el functor silencio.
+* @param +Silencio un silencio
+*/
 es_silencio(silencio(F)) :- es_figura(F).
 
-%ALTURA TONAL
-% la altura de una nota es su nombre y su octava
-es_altura(altura(N,O)) :- es_numNota(N), es_octava(O).
-%la correspondencia es la=0, la#/sib=1, si=2, ...
-es_numNota(numNota(N)) :- member(N,[0,1,2,3,4,5,6,7,8,9,10,11]).
-%el numero de octavas que consideremos, este esta puesto al azar aqui
+
+/**
+* es_altura(+Altura)
+* Definicion de la estructura altura. Basicamente es una pareja formada por el functor altura/2 con un numNota
+* y una octava
+* @param +Altura una altura
+*/
+es_altura(altura(N,O)) :- 
+	es_numNota(N), 
+	es_octava(O).
+
+/**
+* es_numNota(+NumNota)
+* Definicion de la estructura numNota. Representa uno de los 12 sonidos de la octava. Basicamente es un numero 
+* del 0 al 11 (ambos incluidos) con el functor numNota/1.
+* La correspondencia es La = 0, La# =1 , y asi.
+* @param +NumNota una nota de la octava
+*/
+es_numNota(numNota(N)) :- 
+	member(N , [0,1,2,3,4,5,6,7,8,9,10,11]).
+
+/**
+* es_octava(+Octava)
+* Definicion de la estructura octava. Basicamente es un numero con la constructora octava/1. Solo vamos a considerad
+* las octavas de 0 a 6-
+* @param +Octava una octava
+*/
 es_octava(octava(O)) :- member(O,[0,1,2,3,4,5,6]).
 
 %FIGURAS
 %una figura es una fracción, N es el numerador y D el denominador
 es_figura(figura(N,D)) :- es_fraccionNat(N, D).
+
 es_fraccionNat(N, D) :- natural(N), natural(D).
 %natural(N) :- integer(N), N>=0.
 

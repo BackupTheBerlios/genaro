982c7c505fb4a125d624f612238320f173f63312
roberto_____666 <roberto_____666>
2005-03-24 15:15:06 +0000

    Terminado, falta comentar

--- Codigo/Haskell/HaskoreALilypond.hs
+++ Codigo/Haskell/HaskoreALilypond.hs
@@ -1,5 +1,5 @@
 
-module HaskoreALilypond where
+module HaskoreALilypond ( haskoreALilypond, haskoreALilypondString ) where
 
 import HaskoreAMidi
 import Haskore
@@ -9,47 +9,65 @@ import ChildSong6
 import Ratio
 
 
-negrasMinuto :: Int
-negrasMinuto = 120
+type Armadura = (PitchClass, Modo)
+data Modo = Mayor | Menor
+type Ritmo = (Int, Int)
+type Instrumento = String
+data Clave = Sol | Fa
+type Score = (Music, Armadura, Ritmo, Instrumento, Clave)
+
+type Titulo = String
+type Compositor = String
+type CancionLy = (Titulo, Compositor, [Score])
 
 
-musica :: Music
-musica = Trans 1 (
-         ( Note (C, 4) qn [] )  :+: 
-	 ( Note (C, 5) qn [] )  :+:
-	 ( Note (Cf, 5) qn [] ) :+:
-	 ( Rest qn )            :+:
-	 ( Note (Cs, 5) qn [] ) :+:
-         ( Note (D, 4) qn [] )  :+:
-         ( (Note (E, 4) hn [])  :=: (Note (G, 4) qn []) ) :+: 
-         ( Note (A, 4) qn [] ) :+:
-         ( Note (A, 4) qn [] ) :+:
-         ( Note (A, 4) qn [] ) :+:
-         ( Tempo (2%3) ((Note (C, 4) qn [] )  :+: 
-	               ( Note (C, 5) qn [] )  :+:
-	               ( Note (Cf, 5) qn [] ))
-         ))
+haskoreALilypond :: CancionLy -> FilePath -> IO ()
+haskoreALilypond cancionLy archivo = writeFile archivo (haskoreALilypondString cancionLy)
+
+haskoreALilypondString :: CancionLy -> String
+haskoreALilypondString (titulo, compositor, scores) = cadena
+	where cadenaTitulo = "\\header{ title = \"" ++ titulo ++ "\"\ncomposer = \"" ++ compositor ++ "\"}";
+	      cadena = cadenaTitulo ++ deScoresALy scores
 
 
-prueba :: IO ()
-prueba = haskoreAMidi2 musica negrasMinuto ".//borrame.mid"
+deScoresALy :: [Score] -> String
+deScoresALy l = "\n<<\n" ++ concat (map ( (++ "}\n") . ("\\new Staff {\n" ++) . deScoreALy) l) ++ ">>"
+
+
+deScoreALy :: Score -> String
+deScoreALy (music, (pC, modo), (num, den), instrumento, clave) = 
+	cadenaInstrumento ++ "\n" ++ cadenaArmadura ++ "\n" ++ cadenaRitmo ++ "\n" ++ cadenaClave ++ "\n" ++ cadenaMusic ++ "\n"
+	where cadenaMusic = deMusicALy music;
+	      cadenaArmadura = "\\key " ++ imprimePitchClass pC ++ " \\" ++ imprimeModo modo;
+	      cadenaRitmo = "\\time " ++ show num ++ "/" ++ show den;
+	      cadenaClave = "\\clef " ++ imprimeClave clave
+	      cadenaInstrumento = "\\set Staff.instrument = \"" ++ instrumento ++ "\""
+
+imprimeModo :: Modo -> String
+imprimeModo Mayor = "major"
+imprimeModo Menor = "minor"
+
+imprimeClave :: Clave -> String
+imprimeClave Sol = "treble"
+imprimeClave Fa = "bass"
 
 
 deMusicALy :: Music -> String
 deMusicALy (Note p dur _) = imprimeNota p dur
-deMusicALy (Rest dur)      = "r" ++ show (denominator dur)
-deMusicALy (m1 :+: m2)     = " { " ++ deMusicALy m1 ++ " " ++ deMusicALy m2 ++ " } "
-deMusicALy (m1 :=: m2)     = " << " ++ deMusicALy m1 ++ " " ++ deMusicALy m2 ++ " >> " 
-deMusicALy (Tempo d m)       = "\times " ++ imprimeRatio d ++ " { " ++ deMusicALy m ++ " } "
-deMusicALy (Trans t m)       = deMusicALy (suma t m)  -- falta esta por hacer bien
-deMusicALy (Instr _ m)       = deMusicALy m
-deMusicALy (Player _ m)       = deMusicALy m
-deMusicALy (Phrase _ m)       = deMusicALy m
+deMusicALy (Rest dur)     = "r" ++ show (denominator dur)
+deMusicALy (m1 :+: m2)    = " { " ++ deMusicALy m1 ++ " " ++ deMusicALy m2 ++ " } "
+deMusicALy (m1 :=: m2)    = " << " ++ deMusicALy m1 ++ " " ++ deMusicALy m2 ++ " >> " 
+deMusicALy (Tempo d m)    = "\\times " ++ imprimeRatio d ++ " { " ++ deMusicALy m ++ " } "
+deMusicALy (Trans t m)    = deMusicALy (suma t m)  -- falta esta por hacer bien
+deMusicALy (Instr _ m)    = deMusicALy m
+deMusicALy (Player _ m)   = deMusicALy m
+deMusicALy (Phrase _ m)   = deMusicALy m
 
 
 imprimeNota :: Pitch -> Dur -> String
 imprimeNota p dur = eliminaUltimos 2 (concat [imprimePitch p ++ show (denominator dur) ++ "~ " | i <- [1..numerador] ] )
 	where numerador = numerator dur
+
 
 imprimeRatio :: Ratio Int -> String
 imprimeRatio r = show (numerator r) ++ "/" ++ show (denominator r)
@@ -92,3 +110,29 @@ suma t (Player pl m)   = Player pl (suma t m)
 suma t (Phrase l m)    = Phrase l (suma t m)
 
 
+-- EJEMPLO
+
+musica :: Music
+musica = Trans 0 (
+         ( Note (C, 4) qn [] )  :+: 
+	 ( Note (C, 5) qn [] )  :+:
+	 ( Note (Cf, 5) qn [] ) :+:
+	 ( Rest qn )            :+:
+	 ( Note (Cs, 5) qn [] ) :+:
+         ( Note (D, 4) qn [] )  :+:
+         ( (Note (E, 4) hn [])  :=: (Note (G, 4) qn []) ) :+: 
+         ( Note (A, 4) qn [] ) :+:
+         ( Note (A, 4) qn [] ) :+:
+         ( Note (A, 4) qn [] ) :+:
+         ( Tempo (1%1) ((Note (C, 4) qn [] )  :+: 
+	               ( Note (C, 5) qn [] )  :+:
+	               ( Note (Cf, 5) qn [] ))
+         ))
+
+
+{-
+ver en el manual de lilypond \context StaffGroup
+-}
+
+ejemplo :: IO ()
+ejemplo = haskoreALilypond ("Cancioncilla" , "Genaro", [(musica :+: musica :+: musica :+: musica, (C, Menor), (2,4), "Piano", Sol),(musica :+: musica :+: musica :+: musica, (C, Mayor), (2,4), "Bajo", Fa)]) "./borrame.ly"

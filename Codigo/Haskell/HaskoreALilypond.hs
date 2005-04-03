54ce2ad7952adc67d09bad7573e85f89d33ecc4b
roberto_____666 <roberto_____666>
2005-04-03 14:51:00 +0000

    Mejorado, ahora tiene puntillo

--- Codigo/Haskell/HaskoreALilypond.hs
+++ Codigo/Haskell/HaskoreALilypond.hs
@@ -1,17 +1,21 @@
-
-module HaskoreALilypond ( haskoreALilypond, haskoreALilypondString, cancionDef ) where
+ 
+module HaskoreALilypond where
 
 import HaskoreAMidi
 import Haskore
 import Ratio
 import Char
 import Ratio
+import List
+import Maybe
+
 
 -- 'Armadura': especifica la armadura que pondra Lilypond al principio del pentagrama
 type Armadura = (PitchClass, Modo)
 
 -- 'Modo': indica si la armadura y la obra esta en modo mayor o menor
 data Modo = Mayor | Menor
+     deriving (Eq, Ord, Show, Read, Enum)
 
 -- 'Ritmo': ritmo de la obra.
 type Ritmo = (Int  -- Numero de notas del compas
@@ -98,7 +102,7 @@ imprimeClave Fa = "bass"
 -}
 deMusicALy :: Music -> String
 deMusicALy (Note p dur _) = imprimeNota p dur
-deMusicALy (Rest dur)     = "r" ++ show (denominator dur)
+deMusicALy (Rest dur)     = imprimeSilencio dur
 --   Cambia el operador secuencia de Haskore por el secuencia de Lilypond
 deMusicALy (m1 :+: m2)    = " { " ++ deMusicALy m1 ++ " " ++ deMusicALy m2 ++ " } "
 --   Cambia el operador paralelo de Haskore por el paralelo de Lilypond
@@ -112,14 +116,25 @@ deMusicALy (Player _ m)   = deMusicALy m
 deMusicALy (Phrase _ m)   = deMusicALy m
 
 {-
-    'imprimeNota' es cribe una nota en formato Lilypond. En caso de que la duracion de la nota no coincida
+    'imprimeNota' escribe una nota en formato Lilypond. En caso de que la duracion de la nota no coincida
     exactamente con una nota simple (una negra, una redonda, etc.) se escriben tantas notas ligadas como 
     hagan falta.
     En realidad, si la duracion de la nota es una fraccion (Ratio Int) escribimos tantas notas ligadas como
     indique el numerador, cada una de ellas de duracion lo que indique el denominador.
 -}
 imprimeNota :: Pitch -> Dur -> String
-imprimeNota p dur = eliminaUltimos 2 (concat [imprimePitch p ++ show (denominator dur) ++ "~ " | i <- [1..numerador] ] )
+imprimeNota p dur 
+        | numerador == 3 && denominador > 1 = imprimePitch p ++ show (quot denominador 2) ++ "." -- Ponemos puntillo
+        | otherwise      = eliminaUltimos 2 (concat [imprimePitch p ++ show (denominator dur) ++ "~ " | i <- [1..numerador] ] ) -- elimina los dos ultimos para acabar con el ultimo "~"
+	where numerador = numerator dur;
+              denominador = denominator dur;
+
+{-
+   'imprimeSilencio' escribe un silencio en forma lilypond. En caso de que el numerador de la duracion no
+   sea uno repetimos el silencio tantas veces como diga el numerador
+-}
+imprimeSilencio :: Dur -> String 
+imprimeSilencio dur = (concat ["r" ++ show (denominator dur) ++ " " | i <- [1..numerador] ] )
 	where numerador = numerator dur
 
 
@@ -167,7 +182,7 @@ imprimeOctava base oc
     Elimina los n ultimos elementos de una lista
 -}
 eliminaUltimos :: Int -> [a] -> [a]
-eliminaUltimos n l = reverse (drop n (reverse l))
+eliminaUltimos n l = reverse (drop n (reverse l))  -- Si la lista tiene menos elementos que n entonces se devuelve vacia y no sale ningun error
 
 
 {-
@@ -220,4 +235,93 @@ ejemplo2 :: IO ()
 ejemplo2 = haskoreALilypond ("Cancioncilla" , "Genaro", [(mainVoice, (C, Mayor), (2,4), "Pianillo", Sol)]) "./borrame.ly"=======
 ejemplo = haskoreALilypond ("Cancioncilla" , "Genaro", [(musica :+: musica :+: musica :+: musica, (C, Menor), (2,4), "Piano", Sol),(musica :+: musica :+: musica :+: musica, (C, Mayor), (2,4), "Bajo", Fa)]) "./borrame.ly"
 >>>>>>> 1.3
+-}
+
+
+
+arreglaAlteraciones :: Armadura -> Music -> Music
+arreglaAlteraciones armadura
+        | elem armadura modosConSostenidos = soloSostenidos
+        | elem armadura modosConBemoles    = soloBemoles
+                    where modosConSostenidosMayor = zip [C,Cs,D,E,Fs,G,A,B]    [Mayor|i<-[1..]];  -- Los modos sin alteraciones lo consideramos con ellas, como C mayor o A menor
+                          modosConSostenidosMenor = zip [Cs,Ds,E,Fs,Gs,A,As,B] [Menor|i<-[1..]];
+                          modosConBemolesMayor    = zip [Cf,Df,Ef,F,Gf,Af,Bf]  [Mayor|i<-[1..]];
+                          modosConBemolesMenor    = zip [C,D,Ef,F,G,Af,Bf]     [Menor|i<-[1..]];
+                          modosConSostenidos      = modosConSostenidosMayor ++ modosConSostenidosMenor ;
+                          modosConBemoles         = modosConBemolesMayor ++ modosConBemolesMenor
+
+
+soloSostenidos :: Music -> Music
+soloSostenidos (Note p dur l) = Note (cambiaASostenido p) dur l
+soloSostenidos (Rest dur)     = Rest dur
+soloSostenidos (m1 :+: m2)    = soloSostenidos m1 :+: soloSostenidos m2
+soloSostenidos (m1 :=: m2)    = soloSostenidos m1 :=: soloSostenidos m2
+soloSostenidos (Tempo d m)    = Tempo d (soloSostenidos m)
+soloSostenidos (Trans t m)    = Trans t (soloSostenidos m)
+soloSostenidos (Instr i m)    = Instr i (soloSostenidos m)
+soloSostenidos (Player p m)   = Player p (soloSostenidos m)
+soloSostenidos (Phrase p m)   = Phrase p (soloSostenidos m)
+
+
+cambiaASostenido :: Pitch -> Pitch
+cambiaASostenido (pc, o)
+          | pc == Cf                    = (Bs, o - 1)
+          | elem pc listaBemoles        = ([Cs,Ds,E,Fs,Gs,As] !! indice, o)
+          | otherwise                   = (pc, o)
+          where  listaBemoles = [Df,Ef,Ff,Gf,Af,Bf];
+                 indice = fromJust (elemIndex pc listaBemoles)
+
+
+soloBemoles:: Music -> Music
+soloBemoles (Note p dur l) = Note (cambiaABemol p) dur l
+soloBemoles (Rest dur)     = Rest dur
+soloBemoles (m1 :+: m2)    = soloBemoles m1 :+: soloBemoles m2
+soloBemoles (m1 :=: m2)    = soloBemoles m1 :=: soloBemoles m2
+soloBemoles (Tempo d m)    = Tempo d (soloBemoles m)
+soloBemoles (Trans t m)    = Trans t (soloBemoles m)
+soloBemoles (Instr i m)    = Instr i (soloBemoles m)
+soloBemoles (Player p m)   = Player p (soloBemoles m)
+soloBemoles (Phrase p m)   = Phrase p (soloBemoles m)
+
+
+cambiaABemol :: Pitch -> Pitch
+cambiaABemol (pc, o)
+          | pc == Bs                    = (Cf, o + 1)
+          | elem pc listaSostenidos        = ([Df,Ef,F,Gf,Af,Bf] !! indice, o)
+          | otherwise                   = (pc, o)
+          where  listaSostenidos = [Cs,Ds,Es,Fs,Gs,As];
+                 indice = fromJust (elemIndex pc listaSostenidos)
+
+
+
+eliminaSilenciosRedundantes :: Music -> Music
+eliminaSilenciosRedundantes (Note p dur l) = Note p dur l
+eliminaSilenciosRedundantes (Rest dur)     = Rest dur
+eliminaSilenciosRedundantes (m1 :+: m2)    = eliminaSilenciosRedundantes m1 :+: eliminaSilenciosRedundantes m2
+eliminaSilenciosRedundantes (m1 :=: m2)    = compruebaRedundancia (eliminaSilenciosRedundantes m1 :=: eliminaSilenciosRedundantes m2)
+eliminaSilenciosRedundantes (Tempo d m)    = Tempo d  (eliminaSilenciosRedundantes m)
+eliminaSilenciosRedundantes (Trans t m)    = Trans t  (eliminaSilenciosRedundantes m)
+eliminaSilenciosRedundantes (Instr i m)    = Instr i  (eliminaSilenciosRedundantes m)
+eliminaSilenciosRedundantes (Player p m)   = Player p (eliminaSilenciosRedundantes m)
+eliminaSilenciosRedundantes (Phrase p m)   = Phrase p (eliminaSilenciosRedundantes m)
+
+
+compruebaRedundancia :: Music -> Music
+--compruebaRedundancia mu@((m1:+:(Rest d)) :=: m2)
+compruebaRedundancia mu@((Rest d) :=: m)
+       | dur m >= d   = m
+       | otherwise    = mu
+compruebaRedundancia mu@( m :=: (Rest d) )
+       | dur m >= d   = m
+       | otherwise    = mu
+compruebaRedundancia mu@( m1 :=: ((Rest d) :+: m2) )
+       | dur m1 == d  =  m1 :+: m2
+       | otherwise    = mu
+compruebaRedundancia m = m
+
+
+
+{-
+musica4 :: Music
+musica4 = eliminaSilenciosRedundantes ((Note (C,4) (1%2) []) :+: ((Note (C,4) (1%4) []) :=: (Rest (1%5)) :=: (Note (C,4) (1%4) [])))
 -}

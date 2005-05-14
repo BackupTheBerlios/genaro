cfdac330facbf46dac57c9ed210acc5ebe34c920
gatchan <gatchan>
2005-05-14 18:04:30 +0000

    Ahora tiene la funcio que pasa una obra completa a midi

--- Codigo/Haskell/ObraCompleta.hs
+++ Codigo/Haskell/ObraCompleta.hs
@@ -1,7 +1,12 @@
 
 module ObraCompleta where
 
-import HaskoreALilypond(Armadura) 
+import HaskoreALilypond(Armadura, Modo(..)) 
+import Haskore
+import Ratio
+import HaskellAHaskell
+import BiblioGenaro 
+import HaskoreAMidi
 
 
 type Tempo = Int
@@ -23,13 +28,48 @@ type InfoObraCompleta = (Tempo, Armadura)
 type ObraCompleta = (InfoObraCompleta, [Pista])
 
 
-{-
 
 
-deObraCompletaAMidi :: ObraCompleta -> IO ()
+
+
+
+deObraCompletaAMidi :: FilePath -> ObraCompleta -> IO ()
+deObraCompletaAMidi fichDest ((tempo, (pc, Mayor) ), pistas) =
+      do musicas <- mapM dePistaAMusic pistas
+         haskoreAMidi2 (Trans (pitchClass pc) (chordSeguro musicas)) tempo fichDest
+deObraCompletaAMidi _ ((_, (_, Menor) ), _) = error "No tratamos el modo menor"
+
 
 
 deSubbloqueAMusic :: Subbloque -> IO Music
-deSubbloqueAMusic (_, False) = 
+deSubbloqueAMusic (n, _, True) = return (Rest (n%1))
+deSubbloqueAMusic (_, ficheroMusic, False) = 
+      do music <- leeMusic ficheroMusic
+         return music
 
--}
+
+dePistaAMusic :: Pista -> IO Music
+dePistaAMusic ((_ , _, True), subbloques) = 
+      do musicas <- mapM deSubbloqueAMusic subbloques
+         return (Rest (d musicas))
+         where d lista = dur (lineSeguro lista)
+dePistaAMusic ((instr , _, False), subbloques) =
+      do musicas <- mapM deSubbloqueAMusic subbloques
+         return (Instr instr (lineSeguro musicas))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

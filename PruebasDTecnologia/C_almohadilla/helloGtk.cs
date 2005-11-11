771d5795c165da5a9452564e34ff789b0cac20b1
bc24us <bc24us>
2005-11-11 18:55:11 +0000

    hello world simple para gtk# con eventos

---
+++ PruebasDTecnologia/C_almohadilla/helloGtk.cs
@@ -0,0 +1,42 @@
+        using Gtk;
+ 	using System;
+ 
+	// compilar con mcs -pkg:gtk-sharp-2.0 helloGtk.cs
+        class Hello {
+ 
+                static void Main()
+                {
+                        Application.Init ();
+ 
+	                // Set up a button object.
+                        Button btn = new Button ("Hello World");
+                        // when this button is clicked, it'll run hello()
+                        btn.Clicked += new EventHandler (hello);
+ 
+                        Window window = new Window ("helloworld");
+			window.Resize(400,300);
+	                // when this window is deleted, it'll run delete_event()
+                        window.DeleteEvent += delete_event;
+                        
+	                // Add the button to the window and display everything
+	                window.Add (btn);
+                        window.ShowAll ();
+ 
+                        Application.Run ();
+                }
+ 
+ 
+	        // runs when the user deletes the window using the "close
+	        // window" widget in the window frame.
+                static void delete_event (object obj, DeleteEventArgs args)
+                {
+                            Application.Quit ();
+                }
+ 
+                // runs when the button is clicked.
+                static void hello (object obj, EventArgs args)
+                {
+                        Console.WriteLine("Hello World");
+                        Application.Quit ();
+                }
+        }

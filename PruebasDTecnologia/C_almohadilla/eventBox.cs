eef310c7c1012d6b72ebf2f4da3a8ad7458d5140
bc24us <bc24us>
2005-11-20 18:27:30 +0000

    ejemplo de uso de eventBox para widgets no asociados a una ventana

---
+++ PruebasDTecnologia/C_almohadilla/eventBox.cs
@@ -0,0 +1,52 @@
+using Gtk;
+using Gdk;
+using System;
+
+
+public class eventbox
+{
+
+     static void delete_event (object obj, DeleteEventArgs args)
+     {
+          Application.Quit();
+     }
+
+     static void exitbutton_event (object obj, ButtonPressEventArgs args)
+     {
+	  Console.WriteLine("has pulsado una etiqueta, pervertido!!!");
+          Application.Quit();
+     }
+
+     public static void Main (string[] args)
+     {
+          Gtk.Window window;
+          EventBox eventbox;
+          Label label;
+
+          Application.Init();
+
+          window = new Gtk.Window ("Eventbox");
+          window.DeleteEvent += new DeleteEventHandler (delete_event);
+
+          window.BorderWidth = 10;
+	  window.Resize(400,300);
+
+          eventbox = new EventBox ();
+          window.Add (eventbox);
+          eventbox.Show();
+
+          label = new Label ("Click here to quit");
+          eventbox.Add(label);
+          label.Show();
+
+          label.SetSizeRequest(110, 20);
+
+          eventbox.ButtonPressEvent += new ButtonPressEventHandler (exitbutton_event);
+
+          eventbox.Realize();
+
+          window.Show();
+               
+          Application.Run();
+     }
+}

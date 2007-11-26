95fbd7c5b8e8b992f954791336d69f989cc16c3f
bc24us <bc24us>
2007-11-26 15:41:32 +0000

    *** empty log message ***

--- Codigo/Haskell/Haskore.hs
+++ Codigo/Haskell/Haskore.hs
@@ -1,3 +1,6 @@
 module Haskore(module HaskoreLoader) where
 
 import HaskoreLoader
+import Ratio
+
+-- testLinux (cMajArp :=: cMajChd :=: (delay (1%8) cMajChd) :=: (delay (1%8) cMajArp) :=: (delay (2%8) cMajArp))
